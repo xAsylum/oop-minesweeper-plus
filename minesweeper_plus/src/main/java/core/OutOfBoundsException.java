9ddf37fa3d0bffba3bf3cfe54bf75c86d8f6f8df
B8saur <bbromblik@gmail.com>
2024-05-02 12:32:55 +0200

    complete disaster

--- minesweeper_plus/src/main/java/core/OutOfBoundsException.java
+++
@@ -1,5 +0,0 @@
-package core;
-
-public class OutOfBoundsException extends Exception {
-
-}
--- minesweeper_plus/src/main/java/core/Proximity.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-//there could be different types of proximity,
-public interface Proximity {
-    int count(int x, int y, int z);
-}
