0004cdf2223c237db39972fad141d558fd4b5571
B8saur <167637787+B8saur@users.noreply.github.com>
2024-05-02 11:12:59 +0200

    Alter MineException.java

--- minesweeper_plus/src/main/java/core/MineException.java
+++ minesweeper_plus/src/main/java/core/MineException.java
@@ -1,7 +1,12 @@
 package core;
 
 public class MineException extends Exception {
-    int x; int y;
-    MineException(int x, int y){this.x=x; this.y=y;}
-    MineException(){}
+    int x;
+    int y;
+    int z;
+    public MineException (int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
