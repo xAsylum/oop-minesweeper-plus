9fea9088875bbcac4dee7c81bc5325d34da77502
kuba <kuba.chuck2014@gmail.com>
2024-05-02 13:53:10 +0200

    Micek update, v 1.0 ALpha

--- minesweeper_plus/core/src/minesweeper/plus/MyGdxGame.java
+++ minesweeper_plus/core/src/minesweeper/plus/MyGdxGame.java
@@ -1,9 +1,14 @@
 package minesweeper.plus;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {		//to be deleted, only as template
 	SpriteBatch batch;
@@ -12,14 +17,17 @@ public class MyGdxGame extends ApplicationAdapter {		//to be deleted, only as te
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("micek.jpeg");
+
 	}
+
+	Random random = new Random();
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1,1,1,1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, random.nextInt()%200,  random.nextInt()%200);
 		batch.end();
 	}
 	
