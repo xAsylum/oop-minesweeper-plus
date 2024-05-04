516626ab931a4fdab10db8b37556d43f9f49baae
B8saur <bbromblik@gmail.com>
2024-05-04 15:18:04 +0200

    same as before but with fewer warnings

--- minesweeper_plus/core/src/minesweeper/plus/core/Minefield.java
+++ minesweeper_plus/core/src/minesweeper/plus/core/Minefield.java
@@ -6,13 +6,13 @@ import java.util.Set;
 //creates and stores the minefield and answers questions about it
 public interface Minefield {
 
-    public Minefield newField(int width, int height, int depth, int noMines, Coordinates firstGuess) throws OutOfBoundsException;
-    public Coordinates getSize();
+    Minefield newField(int width, int height, int depth, int noMines, Coordinates firstGuess) throws OutOfBoundsException;
+    Coordinates getSize();
 
-    public int clickThis(Coordinates guess) throws MineException, OutOfBoundsException;
+    int clickThis(Coordinates guess) throws MineException, OutOfBoundsException;
                 //returns #mines in proximity of this field
                 //OR MineException if mine OR OutOfBounds if out of bounds
-    public Set<Map.Entry<Coordinates, Integer>> getNeighbourhood (Coordinates guess) throws NotEmptyException, OutOfBoundsException;
+    Set<Map.Entry<Coordinates, Integer>> getNeighbourhood (Coordinates guess) throws NotEmptyException, OutOfBoundsException;
                 //quick-clear empty areas, returns an entrySet
 
 }
