e595c9d715ddb1c391e764432bd1b70dba49d852
B8saur <bbromblik@gmail.com>
2024-06-09 23:34:23 +0200

    cleans

--- minesweeper_plus/core/src/minesweeper/plus/views/Menu.java
+++ minesweeper_plus/core/src/minesweeper/plus/views/Menu.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
