0cf99503db58693d34d09510953e0f1f8dfd8533
xAsylum <jakub.wieliczko@student.uj.edu.pl>
2024-06-09 17:31:33 +0200

    Created ***suwak***

--- minesweeper_plus/core/src/minesweeper/plus/views/Menu.java
+++ minesweeper_plus/core/src/minesweeper/plus/views/Menu.java
@@ -2,6 +2,7 @@ package minesweeper.plus.views;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,8 +18,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import minesweeper.plus.core.Coordinates;
 import minesweeper.plus.core.OutOfBoundsException;
+import minesweeper.plus.services.Board;
 import minesweeper.plus.services.SimpleBoard;
 import minesweeper.plus.viewmodels.SimpleViewModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class Menu extends ScreenAdapter {
 
@@ -32,37 +38,72 @@ public class Menu extends ScreenAdapter {
     boolean printLevel = false;
     boolean createdLevel = false;
 
+    private Consumer<Integer> updateBlock;
+    Map<String, TextButtonStyle> styles;
+
+    void setupStyles() {
+        styles = new HashMap<>();
+        TextButtonStyle playButtonStyle = new TextButtonStyle();
+        playButtonStyle.up = new TextureRegionDrawable(new Texture("menu_button_play.png"));
+        playButtonStyle.down = new TextureRegionDrawable(new Texture("menu_button_play.png"));
+        playButtonStyle.font = new BitmapFont();
+        styles.put("play", playButtonStyle);
+
+        TextButtonStyle settingsButtonStyle = new TextButtonStyle();
+        settingsButtonStyle.up = new TextureRegionDrawable(new Texture("menu_settings_button.png"));
+        settingsButtonStyle.down = new TextureRegionDrawable(new Texture("menu_settings_button.png"));
+        settingsButtonStyle.font = new BitmapFont();
+        styles.put("settings", settingsButtonStyle);
+
+        TextButtonStyle buttonUpStyle = new TextButtonStyle();
+        buttonUpStyle.up = new TextureRegionDrawable(new Texture("game_button_up.png"));
+        buttonUpStyle.down = new TextureRegionDrawable(new Texture("game_button_up.png"));
+        buttonUpStyle.font = new BitmapFont();
+        styles.put("up", buttonUpStyle);
+
+        TextButtonStyle buttonDownStyle = new TextButtonStyle();
+        buttonDownStyle.up = new TextureRegionDrawable(new Texture("game_button_down.png"));
+        buttonDownStyle.down = new TextureRegionDrawable(new Texture("game_button_down.png"));
+        buttonDownStyle.font = new BitmapFont();
+        styles.put("down", buttonDownStyle);
+
+        TextButtonStyle blockStyle = new TextButtonStyle();
+        blockStyle.up = new TextureRegionDrawable(new Texture("menu_button.png"));
+        blockStyle.down = new TextureRegionDrawable(new Texture("menu_button.png"));
+        blockStyle.font = new BitmapFont();
+        styles.put("block", blockStyle);
+    }
+
     void createMenu() {
         menuStage = new Stage();
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         menuStage.act(Gdx.graphics.getDeltaTime());
-
-        table = new Table();
-        table.setFillParent(true);
-        menuStage.addActor(table);
-
-
-        TextButtonStyle style = new TextButtonStyle();
-        style.up = new TextureRegionDrawable(new Texture("menu_button_play.png"));
-        style.down = new TextureRegionDrawable(new Texture("menu_button_play.png"));
-        style.font = new BitmapFont();
-        TextButton button1 = new TextButton("", style);
-        button1.setTransform(true);
-        button1.setScale(10, 8);
-        button1.setX(100);
-        button1.setY(100);
-        button1.addListener(new ClickListener() {
+        TextButton playButton = new TextButton("", styles.get("play"));
+        playButton.setTransform(true);
+        playButton.setWidth(100);
+        playButton.setHeight(100);
+        playButton.setPosition(200, 100);
+        playButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 createdLevel = false;
                 printLevel = true;
             }
         });
-        table.add(button1);
+
+        TextButton settingsButton = new TextButton("", styles.get("settings"));
+        menuStage.addActor(playButton);
+        settingsButton.setTransform(true);
+        settingsButton.setWidth(100);
+        settingsButton.setHeight(100);
+        settingsButton.setPosition(Gdx.graphics.getWidth() - 2 * settingsButton.getWidth() - 100, 100);
+        menuStage.addActor(settingsButton);
 
     }
 
+
+
     void setupLevel() {
-        Gdx.input.setInputProcessor(null);
+        Gdx.input.setInputProcessor(gameMenuStage);
         try {
             Coordinates boardSize = new Coordinates(14, 10, 3);            //change board size here!
             int numberOfMines = 25;            //change number of mines here!
@@ -70,6 +111,8 @@ public class Menu extends ScreenAdapter {
             board = new SimpleBoard(boardSize, numberOfMines);
             view = new SimpleView(new SimpleViewModel(board));
             createdLevel = true;
+            level = 0;
+            updateBlock.accept(0);
         } catch (OutOfBoundsException e) {
             System.out.println("Wrong starting conditions");
             createdLevel = false;
@@ -79,11 +122,53 @@ public class Menu extends ScreenAdapter {
 
     void createGameMenu() {
         gameMenuStage = new Stage();
+
+        TextButton block = new TextButton("", styles.get("block"));
+        block.setTransform(true);
+        block.setWidth(60);
+        block.setHeight(60);
+        block.setPosition(Gdx.graphics.getWidth() - block.getWidth() - 2, Gdx.graphics.getHeight() - 187);
+        updateBlock = (n) -> {
+            block.setPosition(Gdx.graphics.getWidth() - block.getWidth() - 2, (board.getSize().zValue - n - 1) * (Gdx.graphics.getHeight() - 130)/board.getSize().zValue + 60);
+        };
+        gameMenuStage.addActor(block);
+        TextButton upButton = new TextButton("", styles.get("up"));
+        upButton.setTransform(true);
+        upButton.setWidth(60);
+        upButton.setHeight(60);
+        upButton.setPosition(Gdx.graphics.getWidth() - upButton.getWidth() - 2, Gdx.graphics.getHeight() - 130);
+        upButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                --level;
+                if (level < 0) {
+                    level = 0;
+                }
+                updateBlock.accept(level);
+            }
+        });
+        gameMenuStage.addActor(upButton);
+
+        TextButton downButton = new TextButton("", styles.get("down"));
+        downButton.setTransform(true);
+        downButton.setWidth(60);
+        downButton.setHeight(60);
+        downButton.setPosition(Gdx.graphics.getWidth() - upButton.getWidth() - 2, 0);
+        downButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                ++level;
+                if (level >= board.getSize().zValue) {
+                    level = board.getSize().zValue - 1;
+                }
+                updateBlock.accept(level);
+            }
+        });
+
+        gameMenuStage.addActor(downButton);
     }
 
     @Override
     public void show() {
-
+        setupStyles();
         createMenu();
         createGameMenu();
 
@@ -95,18 +180,6 @@ public class Menu extends ScreenAdapter {
     public void render (float delta) {
 
         try {
-            if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
-                ++level;
-                if (level >= board.getSize().zValue) {
-                    level = board.getSize().zValue - 1;
-                }
-            }
-            if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-                --level;
-                if (level < 0) {
-                    level = 0;
-                }
-            }
             if (printLevel) {
                 if(!createdLevel) {
                     setupLevel();
@@ -116,6 +189,8 @@ public class Menu extends ScreenAdapter {
                     Gdx.input.setInputProcessor(menuStage);
                     printLevel = false; // add timer or a button to retry
                 }
+                gameMenuStage.act(delta);
+                gameMenuStage.draw();
             } else {
                 ScreenUtils.clear(255, 255, 255, 0);
 
